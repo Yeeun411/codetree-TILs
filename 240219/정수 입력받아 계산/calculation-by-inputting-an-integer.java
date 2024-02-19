109102b5115bc1932b913b76d67e613bd0918436
강예은 <yeaeun411@naver.com>
2024-02-19 19:26:54 +0900

    Title: 정수 입력받아 계산, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240219/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a*2+3);
+    }
+}
