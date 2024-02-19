78d89f8f881c21e533bfbcf6ae833a415a138bc9
강예은 <yeaeun411@naver.com>
2024-02-19 19:20:54 +0900

    Title: 입력받아 계산, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240219/입력받아 계산/input-calculate.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        a += 2;
+        System.out.println(a);
+    }
+}
