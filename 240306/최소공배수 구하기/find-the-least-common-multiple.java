807775535438852a00798001946a27c402ec20f9
강예은 <yeaeun411@naver.com>
2024-03-06 23:27:48 +0900

    Title: 최소공배수 구하기, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240306/최소공배수 구하기/find-the-least-common-multiple.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        if(n>m){
+            int a = n;
+            n = m;
+            m = a;
+        }
+        int answer = 1;
+        for(int i = 2;i<m;i++){
+            if(n%i==0&&m%i==0){
+                answer *= i;
+                if(answer>=m){
+                    System.out.println(answer);
+                }
+
+            }
+        }
+    }
+}
