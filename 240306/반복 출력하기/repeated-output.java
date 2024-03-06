ebc8592193e37829d57aef9fadfd8a0d2c477856
강예은 <yeaeun411@naver.com>
2024-03-06 22:53:33 +0900

    Title: 반복 출력하기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240306/반복 출력하기/repeated-output.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        for(int i = 0;i<input;i++){
+            System.out.println("12345^&*()_");
+        }
+    }
+}
