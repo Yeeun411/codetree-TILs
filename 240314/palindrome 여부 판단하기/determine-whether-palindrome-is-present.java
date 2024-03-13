918716fca367df050f8d667483693b4dc1c5193a
강예은 <yeaeun411@naver.com>
2024-03-14 01:00:35 +0900

    Title: palindrome 여부 판단하기, Time: 125ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240314/palindrome 여부 판단하기/determine-whether-palindrome-is-present.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String palindrome = scanner.next();
+        char[] arr = palindrome.toCharArray();
+        for(int i=0;i<arr.length;i++){
+            int j = arr.length-i-1;
+            if(i<=j){
+                System.out.println("Yes");
+                break;
+            }else{
+                if(arr[i] != arr[j]){
+                    System.out.println("No");
+                    break;
+                }
+            }
+        }
+    }
+}
