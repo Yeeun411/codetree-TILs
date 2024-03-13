a21e8c1f1f6cdd9b749bc6200dcb0953d453fb55
강예은 <yeaeun411@naver.com>
2024-03-14 01:01:32 +0900

    Title: palindrome 여부 판단하기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240314/palindrome 여부 판단하기/determine-whether-palindrome-is-present.java
+++ 240314/palindrome 여부 판단하기/determine-whether-palindrome-is-present.java
@@ -6,7 +6,7 @@ public class Main {
         char[] arr = palindrome.toCharArray();
         for(int i=0;i<arr.length;i++){
             int j = arr.length-i-1;
-            if(i<=j){
+            if(i>=j){
                 System.out.println("Yes");
                 break;
             }else{
