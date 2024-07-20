c40a45494e5877dabf468a2701c2a3e0e69c01b4
강예은 <yeaeun411@naver.com>
2024-07-20 20:53:18 +0900

    Title: 계속 중첩되는 사각형, Time: 204ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240720/계속 중첩되는 사각형/continuously-overlapping-squares.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int MAX_R = 2000;
+    public static final int OFFSET = 1000;
+
+    public static int[][] checked = new int[MAX_R + 1][MAX_R + 1];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] x1 = new int[n];
+        int[] y1 = new int[n];
+        int[] x2 = new int[n];
+        int[] y2 = new int[n];
+        // 입력
+        for(int i = 0; i < n; i++) {
+            x1[i] = sc.nextInt();
+            y1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+            y2[i] = sc.nextInt();
+
+            // OFFSET을 더해줍니다.
+            x1[i] += OFFSET;
+            y1[i] += OFFSET;
+            x2[i] += OFFSET;
+            y2[i] += OFFSET;
+        }
+
+        for(int i = 0; i < n; i++)
+            for(int x = x1[i]; x < x2[i]; x++)
+                for(int y = y1[i]; y < y2[i]; y++){
+                    if((i%2)==1){
+                        checked[x][y]= 1;
+                    } else checked[x][y]= 0;
+                }
+        
+        int area = 0;
+        for(int x = 0; x <= MAX_R; x++)
+            for(int y = 0; y <= MAX_R; y++)
+                if(checked[x][y] == 1)
+                    area++;
+        
+        // 출력
+        System.out.print(area);
+    }
+}
