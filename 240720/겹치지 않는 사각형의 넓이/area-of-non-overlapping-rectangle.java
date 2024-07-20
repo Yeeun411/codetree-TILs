42a2a7c3c8474ce3f6a9950bfca422f0f12a0c4d
강예은 <yeaeun411@naver.com>
2024-07-20 20:32:15 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 211ms, Memory: 27MB, Status: Passed - CodeTree

--- 240720/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
+++ 240720/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -1,62 +1,51 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static class Rectangle {
-        int x1, y1, x2, y2;
-        Rectangle(int x1, int y1, int x2, int y2) {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
-        }
-    }
+    public static final int N = 3;
+    public static final int MAX_R = 2000;
+    public static final int OFFSET = 1000;
+
+    public static int[] x1 = new int[N];
+    public static int[] y1 = new int[N];
+    public static int[] x2 = new int[N];
+    public static int[] y2 = new int[N];
+
+    public static int[][] checked = new int[MAX_R + 1][MAX_R + 1];
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        List<Rectangle> rectangles = new ArrayList<>();
-        int ax1 = scanner.nextInt();
-        int ay1 = scanner.nextInt();
-        int ax2 = scanner.nextInt();
-        int ay2 = scanner.nextInt();
-        rectangles.add(new Rectangle(ax1, ay1, ax2, ay2));
+        // 입력
+        for(int i = 0; i < N; i++) {
+            x1[i] = sc.nextInt();
+            y1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+            y2[i] = sc.nextInt();
+
+            // OFFSET을 더해줍니다.
+            x1[i] += OFFSET;
+            y1[i] += OFFSET;
+            x2[i] += OFFSET;
+            y2[i] += OFFSET;
+        }
+
+        // 직사각형에 주어진 순으로 1, 2, 3 번호를 붙여줍니다.
+        // 격자 단위로 진행하는 문제이므로
+        // x2, y2에 등호가 들어가지 않음에 유의합니다.
+        for(int i = 0; i < N; i++)
+            for(int x = x1[i]; x < x2[i]; x++)
+                for(int y = y1[i]; y < y2[i]; y++)
+                    checked[x][y]= i + 1;
         
-        int bx1 = scanner.nextInt();
-        int by1 = scanner.nextInt();
-        int bx2 = scanner.nextInt();
-        int by2 = scanner.nextInt();
-        rectangles.add(new Rectangle(bx1, by1, bx2, by2));
+        // 1, 2, 3 순으로 붙였는데도
+        // 아직 숫자 1, 2로 남아있는 영역의 넓이를 구합니다.
+        int area = 0;
+        for(int x = 0; x <= MAX_R; x++)
+            for(int y = 0; y <= MAX_R; y++)
+                if(checked[x][y] == 1 || checked[x][y] == 2)
+                    area++;
         
-        // Read third rectangle for subtraction
-        int cx1 = scanner.nextInt();
-        int cy1 = scanner.nextInt();
-        int cx2 = scanner.nextInt();
-        int cy2 = scanner.nextInt();
-        Rectangle rectC = new Rectangle(cx1, cy1, cx2, cy2);
-
-        Set<String> coveredPoints = new HashSet<>();
-        Set<String> subtractPoints = new HashSet<>();
-
-        for (Rectangle rect : rectangles) {
-            for (int x = rect.x1; x < rect.x2; x++) {
-                for (int y = rect.y1; y < rect.y2; y++) {
-                    coveredPoints.add(x + "," + y);
-                }
-            }
-        }
-
-        // Process points for rectangle C
-        for (int x = rectC.x1; x < rectC.x2; x++) {
-            for (int y = rectC.y1; y < rectC.y2; y++) {
-                subtractPoints.add(x + "," + y);
-            }
-        }
-
-        // Subtract the points of rectangle C from the covered points
-        for (String point : subtractPoints) {
-            coveredPoints.remove(point);
-        }
-
-        System.out.println(coveredPoints.size());
+        // 출력
+        System.out.print(area);
     }
 }
