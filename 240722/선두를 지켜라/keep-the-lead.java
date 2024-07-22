4a49b8e99c616a17648670b974727aaf3e6fdbed
강예은 <yeaeun411@naver.com>
2024-07-22 09:32:14 +0900

    Title: 선두를 지켜라, Time: 265ms, Memory: 28MB, Status: Passed - CodeTree

--- 240722/선두를 지켜라/keep-the-lead.java
+++ 240722/선두를 지켜라/keep-the-lead.java
@@ -1,38 +1,66 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_T = 1000000;
+    
+    public static int n, m;
+    public static int[] posA = new int[MAX_T + 1];
+    public static int[] posB = new int[MAX_T + 1];
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = 0;
-        int b = 0;
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        int[] a_arr = new int[n];
-        int[] b_arr = new int[m];
-        int v = 0;
-        int t = 0;
-
-        for(int i=0;i<n;i++){
-            v = scanner.nextInt();
-            t = scanner.nextInt();
-            a += v*t;
-            a_arr[i] = a;
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
+        
+        // A가 매 초마다 서있는 위치를 기록
+        int timeA = 1;
+        for(int i = 0; i < n; i++) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+            
+            while(t-- > 0) {
+                posA[timeA] = posA[timeA - 1] + v;
+                timeA++;
+            }
         }
-        for(int i=0;i<m;i++){
-            v = scanner.nextInt();
-            t = scanner.nextInt();
-            b += v*t;
-            b_arr[i] = b;
+        
+        // B가 매 초마다 서있는 위치를 기록
+        int timeB = 1;
+        for(int i = 0; i < m; i++) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+            
+            while(t-- > 0) {
+                posB[timeB] = posB[timeB - 1] + v;
+                timeB++;
+            }
         }
-        int count = 1;
-        for(int i=1;i < (m<n?m:n);i++){
-
-            if(a_arr[i]>b_arr[i] && a_arr[i-1]>b_arr[i]){
-
-            } else if(b_arr[i]>a_arr[i] && b_arr[i-1]>a_arr[i]){
-
-            } else{count++;}
+        
+        // A와 B 중 더 앞서 있는 경우를 확인합니다.
+        // A가 리더면 1, B가 리더면 2로 관리합니다.
+        int leader = 0, ans = 0;
+        for(int i = 1; i < timeA; i++) {
+            if(posA[i] > posB[i]) {
+                // 기존 리더가 B였다면
+                // 답을 갱신합니다.
+                if(leader == 2)
+                    ans++;
+                
+                // 리더를 A로 변경합니다.
+                leader = 1; 
+            }
+            else if(posA[i] < posB[i]) {
+                // 기존 리더가 A였다면
+                // 답을 갱신합니다.
+                if(leader == 1)
+                    ans++;
+                
+                // 리더를 B로 변경합니다.
+                leader = 2; 
+            }
         }
-        System.out.println(count);
+        
+        System.out.print(ans);
     }
 }
