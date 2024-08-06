72a3a1a2e2d9b293e1c662b38ec0744525b67269
강예은 <yeaeun411@naver.com>
2024-08-06 09:08:03 +0900

    Title: 체크판위에서 2, Time: 155ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240806/체크판위에서 2/on-the-checkboard-2.java
+++ 240806/체크판위에서 2/on-the-checkboard-2.java
@@ -4,7 +4,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        int[][] arr = new int[n][m];
+        char[][] arr = new char[n][m];
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++){
                 arr[i][j] = scanner.next().charAt(0);
