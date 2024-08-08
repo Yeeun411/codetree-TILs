7428a81d684377e0d80caf576b43b4ef4c348d8b
강예은 <yeaeun411@naver.com>
2024-08-08 09:22:59 +0900

    Title: 거품 정렬 구현, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240808/거품 정렬 구현/implement-bubble-sort.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        bubbleSort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
