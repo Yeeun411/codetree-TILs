9ecca4e9ab9bc0957e7b6c330403ce44afaed851
강예은 <yeaeun411@naver.com>
2024-08-12 04:43:29 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240812/괄호 쌍 만들어주기 2/pair-parentheses-2.java
+++ 240812/괄호 쌍 만들어주기 2/pair-parentheses-2.java
@@ -5,22 +5,31 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String A = scanner.nextLine();
 
-        int openPairs = 0;
-        int closePairs = 0;
+        List<Integer> openIndices = new ArrayList<>();
+        List<Integer> closeIndices = new ArrayList<>();
 
         for (int i = 0; i < A.length() - 1; i++) {
             if (A.charAt(i) == '(' && A.charAt(i + 1) == '(') {
-                openPairs++;
+                openIndices.add(i);
             }
         }
 
         for (int i = 0; i < A.length() - 1; i++) {
             if (A.charAt(i) == ')' && A.charAt(i + 1) == ')') {
-                closePairs++;
+                closeIndices.add(i);
             }
         }
 
-        int result = openPairs * closePairs;
-        System.out.println(result);
+        int count = 0;
+        
+        for (int open : openIndices) {
+            for (int close : closeIndices) {
+                if (open < close) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
     }
 }
