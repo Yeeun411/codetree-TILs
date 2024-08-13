3430508477a2d44251ea923972b1a6a8b8b1acf9
강예은 <yeaeun411@naver.com>
2024-08-13 23:33:47 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240813/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.java
+++ 240813/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.java
@@ -1,23 +1,33 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
         int n = scanner.nextInt();
         int[] arr = new int[n];
-        int max = 0;
-        int dis = 0;
-        for(int i=0;i<n;i++){
+        
+        for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
+        
+        int maxSum = findMaxSum(arr, n);
+        System.out.println(maxSum);
+    }
+    
+    public static int findMaxSum(int[] arr, int n) {
+        if (n < 3) {
+            return 0;
+        }
 
-        for(int i =0;i<n-1;i++){
-            for(int j = 1; j < n;j++){
-                dis = i>j ? i - j : j-i;
-                if(dis != 1){
-                    max = max >= arr[i] + arr[j] ? max : arr[i] + arr[j];
-                }
+        int maxSum = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 2; j < n; j++) {
+                maxSum = Math.max(maxSum, arr[i] + arr[j]);
             }
         }
-        System.out.println(max);
+        
+        return maxSum;
     }
 }
