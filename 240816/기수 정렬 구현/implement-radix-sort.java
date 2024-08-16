ef6ebe9770224c4cf381c1081f0153493feafe49
강예은 <yeaeun411@naver.com>
2024-08-16 14:55:49 +0900

    Title: 기수 정렬 구현, Time: 1327ms, Memory: 51MB, Status: Passed - CodeTree

---
+++ 240816/기수 정렬 구현/implement-radix-sort.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class Main {
+    public static int getMax(int[] arr, int n) {
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void countingSort(int[] arr, int n, int exp) {
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        Arrays.fill(count, 0);
+
+        for (int i = 0; i < n; i++) {
+            int index = (arr[i] / exp) % 10;
+            count[index]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            int index = (arr[i] / exp) % 10;
+            output[count[index] - 1] = arr[i];
+            count[index]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+    }
+
+    public static void radixSort(int[] arr, int n) {
+        int max = getMax(arr, n);
+
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, n, exp);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        radixSort(arr, n);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
